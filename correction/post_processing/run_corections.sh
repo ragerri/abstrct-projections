583be5a266bd369958baa5d0052ae39c9f8dd78d
anaryegen <anaryegen@gmail.com>
2022-11-30 22:54:23 +0100

    update

---
+++ correction/post_processing/run_corections.sh
@@ -0,0 +1,4 @@
+python projection_corrections.py \
+--source_path ../data/neoplasm/dev.tsv \
+--target_path ../data/argument_components/projections/deepl_awesome/neoplasm/dev.tsv \
+--output_path deepl_awesome_pp_dev.tsv 
