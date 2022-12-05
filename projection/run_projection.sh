b23a8592c3e675aa1be9cd9bcf28323370c4ff8e
anaryegen <anaryegen@gmail.com>
2022-12-05 16:12:10 +0100

    update projection

---
+++ projection/run_projection.sh
@@ -0,0 +1,8 @@
+python3 annotation_projection.py \
+--source_test .data/neoplasm/train.tsv \
+--target_test .data/neoplasm/train_deepl.txt \
+--source_augmentation ./biomedical-translation-parallel/en.txt \
+--target_augmentation ./biomedical-translation-parallel/es.txt \
+--output_dir output/ --output_name deepl_train_simalign \
+--do_awesome
+
