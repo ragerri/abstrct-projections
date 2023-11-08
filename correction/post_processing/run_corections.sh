dc21e1e62a318c617d16bbfeb64819f3ae86e109
anaryegen <anaryegen@gmail.com>
2023-11-08 10:43:01 +0100

    minor modification

--- correction/post_processing/run_corections.sh
+++ correction/post_processing/run_corections.sh
@@ -1,8 +1,4 @@
 python projection_corrections.py \
-    --source_path /Users/anaryegen/Desktop/UPV_EHU/PhD/casimedicaos_projections/abstrct_es/abstrct/neoplasm/train.tsv  \
-    --target_path /Users/anaryegen/Desktop/abstrct_train_es.simalign.test.tsv \
-    --output_path /Users/anaryegen/Desktop/train_es_corrected.tsv
-
-# --source_path ../data/neoplasm/dev.tsv \
-# --target_path ../data/argument_components/projections/deepl_awesome/neoplasm/dev.tsv \
-# --output_path deepl_awesome_pp_dev.tsv 
+--source_path ../data/neoplasm/dev.tsv \
+--target_path ../data/argument_components/projections/deepl_awesome/neoplasm/dev.tsv \
+--output_path deepl_awesome_pp_dev.tsv 
